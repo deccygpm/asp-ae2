3390420cf0ae1dabb473805a6e1c81b2e0809aae
Declan McBride <2399448m@student.gla.ac.uk>
2023-03-05 21:30:15 +0000

    concon

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,58 @@
 mod dnslookup;
-mod seqcon;
+
+
+use std::io::{Write, Read};
+use std::net::{TcpStream, IpAddr};
+use std::thread;
+use std::sync::mpsc::channel;
 
 fn main(){
     let dns_addresses = dnslookup::dnslookup();
-    seqcon::seqcon(dns_addresses).expect("Connections failed");
+
+    let (connection_sender, connection_receiver) = channel::<TcpStream>();
+
+    let mut connection_attempts_threads = vec![];
+
+    for _address in &dns_addresses {
+        let (sender, receiver) = channel();
+        let con_sender = connection_sender.clone();
+        connection_attempts_threads.push((sender, thread::spawn(move|| {
+            let message: IpAddr = receiver.recv().unwrap(); 
+            let mut addr = message.to_string();
+            addr.push_str(":80");
+            if let Ok(stream) = TcpStream::connect(addr) {
+                con_sender.send(stream).unwrap();
+            } 
+        })));
+    }
+
+    let connected_client_thread = thread::spawn(move|| {
+        let mut stream = connection_receiver.recv().unwrap();
+        println!("{:?}", stream.peer_addr().unwrap());
+        let mut request = String::new();
+        request.push_str(format!("GET / HTTP/1.1\r\n").as_str());
+        request.push_str(format!("Host: {:?}\r\n", stream.peer_addr().unwrap()).as_str());
+        request.push_str("Connection: close");
+        request.push_str("\r\n");
+        request.push_str("\r\n");
+        let request_bytes = request.as_bytes();
+        stream.write_all(request_bytes).unwrap();
+        let mut response = String::new();
+        stream.read_to_string(&mut response).unwrap();
+        println!("{}", response);  
+    });
+
+    for i in 0..dns_addresses.len() {
+        connection_attempts_threads[i].0.send(dns_addresses[i].ip()).unwrap();
+    }
+
+    connected_client_thread.join().unwrap();
+    for t in connection_attempts_threads{
+        t.1.join().unwrap();
+    }
 }
+ 
+  
+
+  
+
